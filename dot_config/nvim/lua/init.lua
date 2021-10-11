a13035c13afa3b86831b10df0746ef66eee09613
Khadd <dalo2903@gmail.com>
2021-10-11 03:47:23 +0000

    clean up

--- dot_config/nvim/lua/init.lua
+++
@@ -1,9 +0,0 @@
-require('orgmode').setup({
-  org_agenda_files = {'~/Dropbox/org/*'},
-  org_default_notes_file = '~/Dropbox/org/refile.org'
-  })
-require'cmp'.setup({
-  sources = {
-    { name = 'orgmode' }
-  }
-})
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,20 +1,4 @@
 return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-  use { 'tpope/vim-dispatch', cmd = { 'Dispatch', 'Make', 'Focus', 'Start' } }
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use {'kristijanhusak/orgmode.nvim', config = function()
-    require('orgmode').setup{}
-  end
-  }
-  use {
-    "hrsh7th/nvim-cmp",
-    requires = {
-      "hrsh7th/vim-vsnip",
-      "hrsh7th/cmp-buffer",
-    }
-  }
 end)
