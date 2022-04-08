199b3511f38b11cc81353ac19faa54e5f4b546a1
Kha Dinh <dalo2903@gmail.com>
2022-04-08 21:39:40 +0900

    lvim config

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -7,24 +7,165 @@ a global executable or a path to
 an executable
 ]]
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+vim.cmd(":set wrap")
+vim.cmd(":set number relativenumber")
+
+vim.g["neovide_transparency"]=1
+
+-- require("lspconfig").ccls.setup {}
+-- general
+lvim.log.level = "warn"
+lvim.format_on_save = true
+
+
+lvim.builtin.bufferline.tabpages=true
+
+
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+lvim.leader = "space"
+-- add your own keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+
+-- Remap enter to tab
+-- lvim.builtin.cmp.mapping["<Tab>"] = lvim.builtin.cmp.mapping["<CR>"]
+
+lvim.builtin.comment.mappings.extra = true
+
+local _, cmp = pcall(require, "cmp")
+cmp.setup {
+  completion = {
+    completeopt = 'menu,menuone,noinsert',
+    mapping = {
+      ['<tab>'] = cmp.mapping.confirm({ select = true }),
+    }
+  }
+}
+
+-- local _, lightspeed = pcall(require, "lightspeed")
+-- lightspeed.setup {}
+
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+
+local _, actions = pcall(require, "telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+  -- for input mode
+  i = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-n>"] = actions.cycle_history_next,
+    ["<C-p>"] = actions.cycle_history_prev,
+  },
+  -- for normal mode
+  n = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+  },
+}
+
+-- Use which-key to add extra bindings with the leader-key prefix
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+tabs",
+  n = {"<cmd>tabnext<cr>", "next tab"},
+  l = {"<cmd>tabs<cr>", "show tab pages"}
+}
+
+lvim.builtin.which_key.mappings["l"]["h"] = {"<cmd>ClangdSwitchSourceHeader<cr>", "Switch Source/Header"}
+
+lvim.builtin.which_key.mappings["T"] = {
+  name = "+Telescope",
+  b = {"<cmd>Telescope buffers<cr>", "Telescope buffers"},
+  f = {"<cmd>Telescope find_files<cr>", "Telescope find files"},
+  p = {"<cmd>Telescope projects<cr>", "Telescope find projects"},
+  r = {"<cmd>Telescope oldfiles<cr>", "Telescope find recents"},
+  g = {"<cmd>Telescope live_grep<cr>", "Telescope live_grep"},
+}
+lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+
+lvim.builtin.which_key.mappings["/"] =  {"<cmd>Telescope current_buffer_fuzzy_find<cr>", "Fuzzy find current buffer"}
+
+-- lvim.builtin.which_key.mappings["_"] =  {"<cmd>Telescope current_buffer_fuzzy_find<cr>", "Fuzzy find current buffer"}
+-- lvim.keys.normal_mode["<leader>"] = 
+
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
+}
+
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.alpha.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.project.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.show_icons.git = true
+
+require('telescope').load_extension('projects')
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "css",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
+
+
 lvim.plugins = {
-  {"folke/tokyonight.nvim"},
-  {"ThePrimeagen/harpoon"},
+  { "folke/tokyonight.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "ThePrimeagen/harpoon",
+    config = function ()
+      require("telescope").load_extension('harpoon')
+      lvim.builtin.which_key.mappings["m"] = {
+        name = "+Harpoon",
+        f = {"<cmd>lua require('harpoon.mark').add_file()<cr>", "Mark the file"},
+        n = {"<cmd>lua require('harpoon.ui').nav_next()<cr>", "Move to next mark"},
+        p = {"<cmd>lua require('harpoon.ui').nav_prev()<cr>", "Move to previous mark"},
+        t = {"<cmd>Telescope harpoon marks<cr>", "Show marks"},
+        ["1"] = {"<cmd>lua require('harpoon.ui').nav_file(1)<cr>", "Navigate to file 1"},
+        ["2"] = {"<cmd>lua require('harpoon.ui').nav_file(2)<cr>", "Navigate to file 2"},
+        ["3"] = {"<cmd>lua require('harpoon.ui').nav_file(3)<cr>", "Navigate to file 3"},
+        ["4"] = {"<cmd>lua require('harpoon.ui').nav_file(4)<cr>", "Navigate to file 4"}
+      }
+    end,
+  },
   {"preservim/vim-pencil"},
   {"ggandor/lightspeed.nvim"},
   {"kdheepak/lazygit.nvim"},
+  {"rhysd/vim-llvm"},
+  {
+  'lervag/vimtex',
+  config = function ()
+    vim.cmd("syntax enable")
+    -- vim.cmd("let g:vimtex_view_general_viewer = 'okular'")
+    vim.cmd("let g:vimtex_view_method= 'zathura'")
 
-    {
-    'lervag/vimtex',
-    config = function ()
-      vim.cmd("syntax enable")
-      -- vim.cmd("let g:vimtex_view_general_viewer = 'okular'")
-      vim.cmd("let g:vimtex_view_method= 'zathura'")
-      -- vim.cmd("let g:vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'")
-      -- vim.cmd("let g:vimtex_view_general_viewer = 'okular'")
-      -- vim.cmd("let g:vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'")
-      -- vim.cmd("let g:vimtex_view_general_options_latexmk = '--unique'")
-    end,
+    lvim.builtin.which_key.mappings["v"] = {
+      name = "+VimTex",
+      v = {"<cmd>VimtexView<cr>", "View"},
+      c = {"<cmd>VimtexCompile<cr>", "Compile"},
+      C = {"<cmd>VimtexClean<cr>", "Clean"}
+    }
+    -- vim.cmd("let g:vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'")
+    -- vim.cmd("let g:vimtex_view_general_viewer = 'okular'")
+    -- vim.cmd("let g:vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'")
+    -- vim.cmd("let g:vimtex_view_general_options_latexmk = '--unique'")
+  end,
   },
   -- {
   --   "tzachar/cmp-tabnine",
@@ -55,132 +196,14 @@ lvim.plugins = {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
   }
+
 }
 
-vim.cmd(":set wrap")
-vim.cmd(":set number relativenumber")
-
-vim.g["neovide_transparency"]=0.9
-
-require("lightspeed").setup {}
--- require("lspconfig").ccls.setup {}
--- general
-lvim.log.level = "warn"
-lvim.format_on_save = true
-
-lvim.colorscheme = "tokyonight"
-
-lvim.builtin.bufferline.tabpages=true
+-- lvim.colorscheme = "tokyonight"
+lvim.colorscheme = "gruvbox"
+vim.g["gruvbox_contrast_dark"] = "soft"
+vim.g["gruvbox_improved_strings"] = 0
+vim.g["gruvbox_improved_warnings"] = 1
 
 
 
--- keymappings [view all the defaults by pressing <leader>Lk]
-lvim.leader = "space"
--- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-
--- Remap enter to tab
-lvim.builtin.cmp.mapping["<Tab>"] = lvim.builtin.cmp.mapping["<CR>"]
-
-local _, cmp = pcall(require, "cmp")
-cmp.setup {
-  completion = {
-    completeopt = 'menu,menuone,noinsert'
-  }
-}
-
-
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
-
-local _, actions = pcall(require, "telescope.actions")
-lvim.builtin.telescope.defaults.mappings = {
-  -- for input mode
-  i = {
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
-    ["<C-n>"] = actions.cycle_history_next,
-    ["<C-p>"] = actions.cycle_history_prev,
-  },
-  -- for normal mode
-  n = {
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
-  },
-}
-
-
-lvim.builtin.which_key.mappings["v"] = {
-  name = "+VimTex",
-  v = {"<cmd>VimtexView<cr>", "View"},
-  c = {"<cmd>VimtexCompile<cr>", "Compile"},
-  C = {"<cmd>VimtexClean<cr>", "Clean"}
-}
-
-
--- Use which-key to add extra bindings with the leader-key prefix
-lvim.builtin.which_key.mappings["t"] = {
-  name = "+tabs",
-  n = {"<cmd>tabnext<cr>", "next tab"},
-  l = {"<cmd>tabs<cr>", "show tab pages"}
-}
-
-
-
-require("telescope").load_extension('harpoon')
-
-lvim.builtin.which_key.mappings["m"] = {
-  name = "+Harpoon",
-  f = {"<cmd>lua require('harpoon.mark').add_file()<cr>", "Mark the file"},
-  n = {"<cmd>lua require('harpoon.ui').nav_next()<cr>", "Move to next mark"},
-  p = {"<cmd>lua require('harpoon.ui').nav_prev()<cr>", "Move to previous mark"},
-  t = {"<cmd>Telescope harpoon marks<cr>", "Show marks"},
-  ["1"] = {"<cmd>lua require('harpoon.ui').nav_file(1)<cr>", "Navigate to file 1"},
-  ["2"] = {"<cmd>lua require('harpoon.ui').nav_file(2)<cr>", "Navigate to file 2"},
-  ["3"] = {"<cmd>lua require('harpoon.ui').nav_file(3)<cr>", "Navigate to file 3"},
-  ["4"] = {"<cmd>lua require('harpoon.ui').nav_file(4)<cr>", "Navigate to file 4"}
-}
-
-
-lvim.builtin.which_key.mappings["T"] = {
-  name = "+Telescope",
-  b = {"<cmd>Telescope buffers<cr>", "Telescope buffers"},
-  f = {"<cmd>Telescope find_files<cr>", "Telescope find files"},
-  p = {"<cmd>Telescope projects<cr>", "Telescope find projects"},
-  r = {"<cmd>Telescope oldfiles<cr>", "Telescope find recents"}
-}
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
--- }
-
--- TODO: User Config for predefined plugins
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
-lvim.builtin.terminal.active = true
-lvim.builtin.project.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
-
-require('telescope').load_extension('projects')
--- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "css",
-  "yaml",
-}
-
-lvim.builtin.treesitter.ignore_install = { "haskell" }
-lvim.builtin.treesitter.highlight.enabled = true
