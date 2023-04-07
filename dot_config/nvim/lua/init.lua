1694e12be0f2d0a89effb258c2171945bf7ca26b
Kha Dinh <dalo2903@gmail.com>
2023-04-07 16:59:28 +0900

    lazyvim config

--- dot_config/nvim/lua/init.lua
+++
@@ -1,4 +0,0 @@
-vim.o.background = "dark" 
-vim.cmd([[colorscheme gruvbox]])
-require("gruvbox").setup({contrast="soft"})
-
---
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,47 @@
+return {
+  -- add symbols-outline
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    opts = {
+      -- add your options that should be passed to the setup() function here
+      position = "right",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    autoformat = false,
+    servers = {
+      setup = {
+        -- example to setup with typescript.nvim
+        -- tsserver = function(_, opts)
+        --   require("typescript").setup({ server = opts })
+        --   return true
+        -- end,
+        -- Specify * to use this function as a fallback for any server
+        -- ["*"] = function(server, opts) end,
+        -- Fix clangd offset encoding  https://www.lazyvim.org/configuration/recipes
+        clangd = function(_, opts)
+          opts.capabilities.offsetEncoding = { "utf-16" }
+        end,
+      },
+    },
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    config = function()
+      local rt = require("rust-tools")
+      rt.setup({
+        server = {
+          on_attach = function(_, bufnr)
+            -- Hover actions
+            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+          end,
+        },
+      })
+    end,
+  },
+}
