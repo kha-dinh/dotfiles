1695808761219c4dc1764a5dcbb2a68ab14c68f0
Kha Dinh <dalo2903@gmail.com>
2023-09-05 17:04:04 +0900

    up

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
 
   set  -gx ZK_NOTEBOOK_DIR "$HOME/mynotes"
   atuin init fish | source
+  fish_vi_key_bindings
     # Commands to run in interactive sessions can go here
 end
