77835bab188fd3e19a8fbdf604077ac40a108b7a
khadinh <dalo2903@gmail.com>
2024-05-20 16:27:54 +0900

    .

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -1,34 +1,34 @@
 return {
-  {
-    "epwalsh/obsidian.nvim",
-    version = "*",
-    lazy = true,
-    ft = { "pandoc", "markdown" },
-    keys = {
-      { "<leader>Of", "<cmd>ObsidianQuickSwitch<cr>", desc = "Open file" },
-      { "<leader>Oo", "<cmd>ObsidianOpen<cr>", desc = "Open in Obsidian App" },
-      { "<leader>O/", "<cmd>ObsidianSearch<cr>", desc = "Search" },
-      { "<leader>On", ":ObsidianNew ", mode = "n", desc = "Create New Obsidian Note" },
-    },
-    opts = {
-      workspaces = {
-        {
-          name = "My Vault",
-          path = "~/obsidian/vault",
-        },
-      },
-    },
-    -- config = function()
-    --   require("obsidian").setup({
-    --
-    --     dir = "/home/khadd/obsidian/vault",
-    --     note_id_func = function(title)
-    --       return title
-    --     end,
-    --     completion = {
-    --       nvim_cmp = true, -- if using nvim-cmp, otherwise set to false
-    --     },
-    --   })
-    -- end,
-  },
+  -- {
+  --   "epwalsh/obsidian.nvim",
+  --   version = "*",
+  --   lazy = true,
+  --   ft = { "pandoc", "markdown" },
+  --   keys = {
+  --     { "<leader>Of", "<cmd>ObsidianQuickSwitch<cr>", desc = "Open file" },
+  --     { "<leader>Oo", "<cmd>ObsidianOpen<cr>", desc = "Open in Obsidian App" },
+  --     { "<leader>O/", "<cmd>ObsidianSearch<cr>", desc = "Search" },
+  --     { "<leader>On", ":ObsidianNew ", mode = "n", desc = "Create New Obsidian Note" },
+  --   },
+  --   opts = {
+  --     workspaces = {
+  --       {
+  --         name = "My Vault",
+  --         path = "~/obsidian/vault",
+  --       },
+  --     },
+  --   },
+  --   -- config = function()
+  --   --   require("obsidian").setup({
+  --   --
+  --   --     dir = "/home/khadd/obsidian/vault",
+  --   --     note_id_func = function(title)
+  --   --       return title
+  --   --     end,
+  --   --     completion = {
+  --   --       nvim_cmp = true, -- if using nvim-cmp, otherwise set to false
+  --   --     },
+  --   --   })
+  --   -- end,
+  -- },
 }
