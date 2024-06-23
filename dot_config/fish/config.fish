20bd0856287ddb3c29ea958f0620555813b5f0af
khadinh <dalo2903@gmail.com>
2024-06-23 16:52:47 +0900

    fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,7 +1,8 @@
 if status is-interactive
-  set  -gx ZK_NOTEBOOK_DIR "$HOME/mynotes"
-  atuin init fish | source
-  fish_vi_key_bindings
+    set -gx ZK_NOTEBOOK_DIR "$HOME/mynotes"
+    atuin init fish | source
+    fish_vi_key_bindings
     # Commands to run in interactive sessions can go here
-  source ~/.iterm2_shell_integration.fish
+    source ~/.iterm2_shell_integration.fish
+    fish_add_path ~/.scripts
 end
