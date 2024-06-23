088ebd2505c865541d8369fa662c2b91cfa2fbdb
Kha Dinh <dalo2903@gmail.com>
2024-06-23 17:05:20 +0900

    set zk dir

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,7 +1,12 @@
 if status is-interactive
-  set  -gx ZK_NOTEBOOK_DIR "$HOME/mynotes"
-  atuin init fish | source
-  fish_vi_key_bindings
+    set -gx ZK_NOTEBOOK_DIR "$HOME/mynotes"
+    set -gx VISUAL neovide
+    set -gx EDITOR nvim
+    atuin init fish | source
+    fish_vi_key_bindings
     # Commands to run in interactive sessions can go here
-  source ~/.iterm2_shell_integration.fish
+    source ~/.iterm2_shell_integration.fish
 end
+
+# Created by `pipx` on 2024-06-21 12:10:51
+set PATH $PATH /home/khadd/.local/bin
