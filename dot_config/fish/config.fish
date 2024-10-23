151dc17a498d0e78d1daa5cb777c3e19c153d732
khadinh <dalo2903@gmail.com>
2024-10-23 16:31:45 +0900

    gtfo

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,10 +3,13 @@ if status is-interactive
     set -gx VISUAL neovide
     set -gx EDITOR nvim
     atuin init fish | source
+    zoxide init fish | source
     fish_vi_key_bindings
     # Commands to run in interactive sessions can go here
     source ~/.iterm2_shell_integration.fish
     fish_add_path ~/.scripts
+    fish_add_path /opt/homebrew/bin
+    fish_add_path /Library/TeX/texbin
 end
 
 # Created by `pipx` on 2024-06-21 12:10:51
